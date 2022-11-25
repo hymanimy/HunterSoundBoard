7462820f89bc734357b1ff06863646e2709b3d9d
hymanimy <46727207+hymanimy@users.noreply.github.com>
2022-11-25 19:44:25 +0000

    Delete HunterSoundBoard directory

--- HunterSoundBoard/hunter_app.js
+++
@@ -1,9 +0,0 @@
-function playAudio(filename){
-    let audio = new Audio(filename)
-    audio.play()
-}
-
-function changeBackground(filename){
-    document.body.style.backgroundImage = 'url(' + filename + ')'
-    document.body.style.backgroundSize  = '300px'
-}
--- HunterSoundBoard/kat_app.js
+++
@@ -1,6 +0,0 @@
-function playLoop(){
-    let an_hour_audio = new Audio("sounds/anhour.m4a")
-    an_hour_audio.loop = true; 
-    an_hour_audio.play()
-
-}
